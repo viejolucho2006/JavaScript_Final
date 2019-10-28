b0911cd0796fdc1e0987389fccf76ef7639554f3
luis_lopez <luis_lopez23@hotmail.com>
2019-10-27 20:53:13 -0500

    Adaptar botones
    Se configura el evento click para los botones del 0 al 9

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@ var operacion;
 var uno   = document.getElementById('1');
 var dos   = document.getElementById('2');
 var tres   = document.getElementById('3');
-var cuatro   = document.getElementById('1');
-var cinco   = document.getElementById('2');
-var seis   = document.getElementById('3');
-var siete   = document.getElementById('2');
-var ocho   = document.getElementById('3');
-var nueve   = document.getElementById('1');
-var cero   = document.getElementById('2');
+var cuatro   = document.getElementById('4');
+var cinco   = document.getElementById('5');
+var seis   = document.getElementById('6');
+var siete   = document.getElementById('7');
+var ocho   = document.getElementById('8');
+var nueve   = document.getElementById('9');
+var cero   = document.getElementById('0');
 
 
 uno.addEventListener('mousedown', function() {
