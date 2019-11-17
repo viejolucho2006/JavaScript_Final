18c5c78b2252c8cb7d402207836b14d3885e31f1
luis_lopez <luis_lopez23@hotmail.com>
2019-11-17 11:52:18 -0500

    Replicacion control de cero
    Se escribe la validadcion para cada elemento nuemerico excepto el cero

--- js/app.js
+++ js/app.js
@@ -99,46 +99,90 @@ cero.addEventListener('mousedown', function() {
 //cargar los numeros para la operacion	
 
 uno.onclick = function(e){
-		 if(x1==0){
-			resultado.textContent = "1";
-			x1=1;
-			 }else{
-			resultado.textContent = resultado.textContent  + "1";
-		 }
-	  }
+	if(x1==0){
+		resultado.textContent = "1";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "1";
+		}
+}
 	  
 dos.onclick = function(e){
-          	 if(x1==0){
-			resultado.textContent = "2";
-			x1=1;
-			 }else{
-			resultado.textContent = resultado.textContent  + "2";
-		 }
-      }
+	if(x1==0){
+		resultado.textContent = "2";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "2";
+		}
+}
+
 tres.onclick = function(e){
-          resultado.textContent = resultado.textContent  + "3";
-      }
+	if(x1==0){
+		resultado.textContent = "3";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "3";
+		}
+}
+
 cuatro.onclick = function(e){
-          display.textContent = display.textContent  + "4";
-      }
+	if(x1==0){
+		resultado.textContent = "4";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "4";
+		}
+}
+
 cinco.onclick = function(e){
-          resultado.textContent = resultado.textContent  + "5";
-      }
+	if(x1==0){
+		resultado.textContent = "5";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "5";
+		}
+}
+
 seis.onclick = function(e){
-          resultado.textContent = resultado.textContent  + "6";
-      }
+	if(x1==0){
+		resultado.textContent = "6";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "6";
+		}
+}
+
 siete.onclick = function(e){
-          resultado.textContent = resultado.textContent  + "7";
-      }
+	if(x1==0){
+		resultado.textContent = "7";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "7";
+		}
+}
+
 ocho.onclick = function(e){
-          resultado.textContent = resultado.textContent  + "8";
-      }
+	if(x1==0){
+		resultado.textContent = "8";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "8";
+		}
+}
+
+	  
 nueve.onclick = function(e){
-          resultado.textContent = resultado.textContent  + "9";
-      }
+	if(x1==0){
+		resultado.textContent = "9";
+		x1=1;
+	}else{
+		resultado.textContent = resultado.textContent  + "9";
+		}
+}
+
 cero.onclick = function(e){
           resultado.textContent = display.textContent  + "0";
-      }
+}
 	  	  		  		  	
 on.onclick = function(e){
           resetear();
