d3e8a8008847829d052dd522827cc403b6ad00c3
luis_lopez <luis_lopez23@hotmail.com>
2020-03-28 21:55:08 -0500

    Update app.js
    Cambiar codigo para inlcuir patron módulo, revisar variables y ajustar funciones para cambio de tamano en botones.

--- js/app.js
+++ js/app.js
@@ -1,170 +1,178 @@
-//Declaramos variables de operacion
-var operandoa=0;
-var operandob=0;
-var operacion=0;
-var pantalla=0;	
-x1=0; //variable para validar cero antes de un numero
-x2=0; //validador del signo
+var calculadora = {
 
-//declaramos variables
-function init(){
-var pantalla = document.getElementById('display');
-var reset = document.getElementById('on');
-var signo = document.getElementById('sign');
-var punto  = document.getElementById('punto');
-var suma = document.getElementById('mas');
-var resta = document.getElementById('menos');
-var multiplicacion = document.getElementById('por');
-var division = document.getElementById('dividido');
-var raiz = document.getElementById('raiz');
-var igual = document.getElementById('igual');	
-var uno    = document.getElementById('1');
-var dos    = document.getElementById('2');
-var tres   = document.getElementById('3');
-var cuatro = document.getElementById('4');
-var cinco  = document.getElementById('5');
-var seis   = document.getElementById('6');
-var siete  = document.getElementById('7');
-var ocho   = document.getElementById('8');
-var nueve  = document.getElementById('9');
-var cero   = document.getElementById('0');
-
-
-
-uno.addEventListener('mousedown', function(e) {
-		document.getElementById('1').style="width:29%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('1').style="width:29%;height: 62.91px;";
-		});
-	});
-	  
-dos.addEventListener('mousedown', function(e) {
-		document.getElementById('2').style="width:29%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('2').style="width:29%;height: 62.91px;";
-		});
-	});
+//Declaramos variables globales
+	pantalla: document.getElementById("display"),
+	valorPantalla: "0",
+	operacion: "",
+	operandoA: 0,
+	operandoB: 0,
+	operandoC: 0,
+	resultado: 0,
+	masIgual: false, // Para permitir ingreso consecutivo
 	
-tres.addEventListener('mousedown', function() {
-		document.getElementById('3').style="width:29%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('3').style="width:29%;height: 62.91px;";
-		});
-	});	
+	init:(function(){
+		this.CambioBotones(".tecla")
+		this.numeros();
+	}),
 
-cuatro.addEventListener('mousedown', function() {
-		document.getElementById('4').style="width:22%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('4').style="width:22%;height: 62.91px;";
+//Cambiar tamaño de botones
+	CambioBotones:function(selector){
+		var uno    = document.getElementById('1');	
+		var dos    = document.getElementById('2');
+		var tres   = document.getElementById('3');
+		var cuatro = document.getElementById('4');
+		var cinco  = document.getElementById('5');
+		var seis   = document.getElementById('6');
+		var siete  = document.getElementById('7');
+		var ocho   = document.getElementById('8');
+		var nueve  = document.getElementById('9');
+		var cero   = document.getElementById('0');
+		var on_c = document.getElementById('on');
+		var signo = document.getElementById('sign');
+		var punto  = document.getElementById('punto');
+		var suma = document.getElementById('mas');
+		var resta = document.getElementById('menos');
+		var multiplicacion = document.getElementById('por');
+		var division = document.getElementById('dividido');
+		var raiz = document.getElementById('raiz');
+		var igual = document.getElementById('igual');	
+		
+		uno.addEventListener('mousedown', function(e) {
+			document.getElementById('1').style="width:29%;height: 62.50px;";
+			addEventListener('mouseup', function(e) {
+			document.getElementById('1').style="width:29%;height: 62.91px;";
+			});
 		});
-	});
-	  
-cinco.addEventListener('mousedown', function() {
-		document.getElementById('5').style="width:22%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('5').style="width:22%;height: 62.91px;";
+		
+		dos.addEventListener('mousedown', function(e) {
+			document.getElementById('2').style="width:29%;height: 62.50px;";
+			addEventListener('mouseup', function(e) {
+			document.getElementById('2').style="width:29%;height: 62.91px;";
+			});
 		});
-	});
-	
-seis.addEventListener('mousedown', function() {
-		document.getElementById('6').style="width:22%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('6').style="width:22%;height: 62.91px;";
-		});
-	});	
+		
+		tres.addEventListener('mousedown', function(e) {
+			document.getElementById('3').style="width:29%;height: 62.50px;";
+			addEventListener('mouseup', function(e) {
+			document.getElementById('3').style="width:29%;height: 62.91px;";
+			});
+		});	
 
-siete.addEventListener('mousedown', function() {
-		document.getElementById('7').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('7').style="width:22%;height: 62.91px;";
-		});
-	});
-	  
-ocho.addEventListener('mousedown', function() {
-		document.getElementById('8').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('8').style="width:22%;height: 62.91px;";
-		});
-	});
-	
-nueve.addEventListener('mousedown', function() {
-		document.getElementById('9').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('9').style="width:22%;height: 62.91px;";
-		});
-	});	
-cero.addEventListener('mousedown', function() {
-		document.getElementById('0').style="width:29%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('0').style="width:29%;height: 62.91px;";
-		});
-	});	
+		cuatro.addEventListener('mousedown', function(e) {
+				document.getElementById('4').style="width:22%;height: 62.50px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('4').style="width:22%;height: 62.91px;";
+				});
+			});
+		  
+		cinco.addEventListener('mousedown', function(e) {
+				document.getElementById('5').style="width:22%;height: 62.50px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('5').style="width:22%;height: 62.91px;";
+				});
+			});
+			
+		seis.addEventListener('mousedown', function(e) {
+				document.getElementById('6').style="width:22%;height: 62.50px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('6').style="width:22%;height: 62.91px;";
+				});
+			});	
 
-punto.addEventListener('mousedown', function() {
-		document.getElementById('punto').style="width:29%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('punto').style="width:29%;height: 62.91px;";
-		});
-	});	
-	
-igual.addEventListener('mousedown', function() {
-		document.getElementById('igual').style="width:29%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('igual').style="width:29%;height: 62.91px;";
-		});
-	});	
-	
-	
-suma.addEventListener('mousedown', function() {
-		document.getElementById('mas').style="width:90%;height: 99%;";
-		addEventListener('mouseup', function() {
-		document.getElementById('mas').style="width:90%;height: 100%;";
-		});
-	});		
-	
-resta.addEventListener('mousedown', function() {
-		document.getElementById('menos').style="width:22%;height: 62.50px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('menos').style="width:22%;height: 62.91px;";
-		});
-	});	
+		siete.addEventListener('mousedown', function(e) {
+				document.getElementById('7').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('7').style="width:22%;height: 62.91px;";
+				});
+			});
+			  
+		ocho.addEventListener('mousedown', function(e) {
+				document.getElementById('8').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('8').style="width:22%;height: 62.91px;";
+				});
+			});
+			
+		nueve.addEventListener('mousedown', function(e) {
+				document.getElementById('9').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('9').style="width:22%;height: 62.91px;";
+				});
+			});	
+		cero.addEventListener('mousedown', function(e) {
+				document.getElementById('0').style="width:29%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('0').style="width:29%;height: 62.91px;";
+				});
+			});	
 
-multiplicacion.addEventListener('mousedown', function() {
-		document.getElementById('por').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('por').style="width:22%;height: 62.91px;";
-		});
-	});		
+		punto.addEventListener('mousedown', function(e) {
+				document.getElementById('punto').style="width:29%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('punto').style="width:29%;height: 62.91px;";
+				});
+			});	
+			
+		igual.addEventListener('mousedown', function(e) {
+				document.getElementById('igual').style="width:29%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('igual').style="width:29%;height: 62.91px;";
+				});
+			});	
+			
+			
+		suma.addEventListener('mousedown', function(e) {
+				document.getElementById('mas').style="width:90%;height: 99%;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('mas').style="width:90%;height: 100%;";
+				});
+			});		
+			
+		resta.addEventListener('mousedown', function(e) {
+				document.getElementById('menos').style="width:22%;height: 62.50px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('menos').style="width:22%;height: 62.91px;";
+				});
+			});	
 
-reset.addEventListener('mousedown', function() {
-		document.getElementById('on').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('on').style="width:22%;height: 62.91px;";
-		});
-	});		
+		multiplicacion.addEventListener('mousedown', function(e) {
+				document.getElementById('por').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('por').style="width:22%;height: 62.91px;";
+				});
+			});		
 
-signo.addEventListener('mousedown', function() {
-		document.getElementById('sign').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('sign').style="width:22%;height: 62.91px;";
-		});
-	});		
-	
-	
-raiz.addEventListener('mousedown', function() {
-		document.getElementById('raiz').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('raiz').style="width:22%;height: 62.91px;";
-		});
-	});		
+		on_c.addEventListener('mousedown', function(e) {
+				document.getElementById('on').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('on').style="width:22%;height: 62.91px;";
+				});
+			});		
 
-	
-division.addEventListener('mousedown', function() {
-		document.getElementById('dividido').style="width:22%;height: 62px;";
-		addEventListener('mouseup', function() {
-		document.getElementById('dividido').style="width:22%;height: 62.91px;";
-		});
-	});		
+		signo.addEventListener('mousedown', function(e) {
+				document.getElementById('sign').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('sign').style="width:22%;height: 62.91px;";
+				});
+			});		
+			
+			
+		raiz.addEventListener('mousedown', function(e) {
+				document.getElementById('raiz').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('raiz').style="width:22%;height: 62.91px;";
+				});
+			});		
+
+			
+		division.addEventListener('mousedown', function(e) {
+				document.getElementById('dividido').style="width:22%;height: 62px;";
+				addEventListener('mouseup', function(e) {
+				document.getElementById('dividido').style="width:22%;height: 62.91px;";
+				});
+			});		
+	},
+	//Fin Cambiar tamaño de botones
 	
 //cargar los numeros para la operacion	
 
@@ -390,6 +398,8 @@ function resetear(){
 
 	
 }		
-	
+
+
+}	
 	
 	
